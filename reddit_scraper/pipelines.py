1c4e0848ab2fafecc5ee1e6aab42b8213fea31b7
Farahad Khurami <fkhurami98@gmail.com>
2024-10-13 19:02:00 +0100

    Add pipeline for post_full_content spider and refactor otehr pipelines

--- reddit_scraper/pipelines.py
+++ reddit_scraper/pipelines.py
@@ -4,14 +4,39 @@ from datetime import datetime
 from urllib.parse import urlparse
 
 
+def _convert_to_int(value):
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        return None
+
+
+def _format_timestamp(timestamp: str) -> str:
+    try:
+        parsed_time = datetime.fromisoformat(timestamp)
+        return parsed_time.strftime("%Y-%m-%d %H:%M:%S")
+    except Exception:
+        return timestamp
+
+
+def _generate_filename(url: str, output_dir: str) -> str:
+    parsed_url = urlparse(url)
+    filename = f"{parsed_url.netloc}{parsed_url.path}".replace("/", "_") + ".json"
+    return os.path.join(output_dir, filename)
+
+
 class SubredditListGenSpiderPipeline:
+    """
+    Pipeline for subreddit_list_gen spider.
+    """
+
     def open_spider(self, spider):
         self.subreddit_urls = []
 
     def close_spider(self, spider):
         with open("reddit_scraper/spiders/constants/start_urls.py", "w") as f:
             f.write(
-                "# Automatically generated list of subreddit start URLs. Run the subreddit_list_gen spider to get started.;\n"
+                "# Automatically generated list of subreddit start URLs. Run the subreddit_list_gen spider to get started.\n"
             )
             f.write("START_URLS = [\n")
             for url in self.subreddit_urls:
@@ -26,20 +51,12 @@ class SubredditListGenSpiderPipeline:
 
 
 class SubredditPostMetaSpiderPipeline:
-    def _format_timestamp(self, timestamp: str) -> str:
-        try:
-            parsed_time = datetime.fromisoformat(timestamp)
-            return parsed_time.strftime("%Y-%m-%d %H:%M:%S")
-        except Exception as e:
-            return timestamp
-
-    def _generate_filename(self, url: str) -> str:
-        parsed_url = urlparse(url)
-        filename = f"{parsed_url.netloc}{parsed_url.path}".replace("/", "_") + ".json"
-        return os.path.join(self.output_dir, filename)
+    """
+    Pipeline for subreddit_post_meta spider.
+    """
 
     def open_spider(self, spider):
-        self.output_dir = "json_files"
+        self.output_dir = "subreddit_post_meta_json_files"
         os.makedirs(self.output_dir, exist_ok=True)
         self.files = {}
 
@@ -50,16 +67,62 @@ class SubredditPostMetaSpiderPipeline:
             file.close()
 
     def process_item(self, item, spider):
+
         if "created_timestamp" in item:
-            item["created_timestamp"] = self._format_timestamp(
-                item["created_timestamp"]
-            )
+            item["created_timestamp"] = _format_timestamp(item["created_timestamp"])
+
+        if "comments" in item:
+            item["comments"] = _convert_to_int(item["comments"])
 
         if "permalink" in item:
-            item["permalink"] = f"https://www.reddit.com{item["permalink"]}"
+            item["permalink"] = f"https://www.reddit.com{item['permalink']}"
 
         start_url = item.get("start_url")
-        filename = self._generate_filename(start_url)
+        filename = _generate_filename(start_url, self.output_dir)
+
+        if filename not in self.files:
+            self.files[filename] = open(filename, "w", encoding="utf-8")
+            self.files[filename].write("[\n")
+
+        json.dump(dict(item), self.files[filename], ensure_ascii=False, indent=4)
+        self.files[filename].write(",\n")
+
+        return item
+
+
+class PostFullContentSpiderPipeline:
+    """
+    Pipeline for post_full_content spider.
+    """
+
+    def open_spider(self, spider):
+        self.output_dir = "post_full_content_json_files"
+        os.makedirs(self.output_dir, exist_ok=True)
+        self.files = {}
+
+    def close_spider(self, spider):
+        for file in self.files.values():
+            file.seek(file.tell() - 2, os.SEEK_SET)
+            file.write("\n]")
+            file.close()
+
+    def process_item(self, item, spider):
+
+        if "post_upvotes" in item:
+            item["post_upvotes"] = _convert_to_int(item["post_upvotes"])
+
+        if "post_comment_count" in item:
+            item["post_comment_count"] = _convert_to_int(item["post_comment_count"])
+
+        if "comments" in item:
+            for comment in item["comments"]:
+                if "comment_upvotes" in comment:
+                    comment["comment_upvotes"] = _convert_to_int(
+                        comment["comment_upvotes"]
+                    )
+
+        start_url = item.get("start_url", item.get("post_permalink"))
+        filename = _generate_filename(start_url, self.output_dir)
 
         if filename not in self.files:
             self.files[filename] = open(filename, "w", encoding="utf-8")
