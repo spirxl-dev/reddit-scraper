ab286bfc6287a6da70547ca557aaf9e033ef3891
Farahad Khurami <fkhurami98@gmail.com>
2024-11-28 18:39:25 +0000

    bruh

--- database/subreddit_urls_to_db.py
+++ database/subreddit_urls_to_db.py
@@ -2,10 +2,10 @@ import sqlite3
 
 # Config
 DEFAULT_PATH = "/new"  # Default subreddit path
-ENABLE_HOT = False     # Include /hot
+ENABLE_HOT = False      # Include /hot
 ENABLE_TOP = False     # Include /top with variations
 
-db_path = "database/data.db"
+db_path = "data.db"
 
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
@@ -36,15 +36,16 @@ base_subreddit_urls = [
 
 subreddit_urls = []
 for base_url in base_subreddit_urls:
-    subreddit_urls.append(f"{base_url}{DEFAULT_PATH}")
+    base_json_url = f"{base_url}/.json?limit=100"
+    subreddit_urls.append(base_json_url)
     if ENABLE_HOT:
-        subreddit_urls.append(f"{base_url}/hot")
+        subreddit_urls.append(f"{base_json_url}/hot")
     if ENABLE_TOP:
-        subreddit_urls.append(f"{base_url}/top")
-        subreddit_urls.append(f"{base_url}/top/?t=all")
-        subreddit_urls.append(f"{base_url}/top/?t=year")
-        subreddit_urls.append(f"{base_url}/top/?t=month")
-        subreddit_urls.append(f"{base_url}/top/?t=day")
+        subreddit_urls.append(f"{base_json_url}/top/?t=all")
+        # subreddit_urls.append(f"{base_json_url}/top")
+        # subreddit_urls.append(f"{base_json_url}/top/?t=year")
+        # subreddit_urls.append(f"{base_json_url}/top/?t=month")
+        # subreddit_urls.append(f"{base_json_url}/top/?t=day")
 
 for url in subreddit_urls:
     try:
